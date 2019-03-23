96e1926d2fd2bf92e250884aa9ace1c3a7e36a14
admin <admin>
2019-03-23 15:40:53 +0800

    Jay 1.自定义view手势滑动绘制框(FingerSlideFrameView)

--- app/src/main/java/com/jay/exersiceinputdemo/MainActivity.java
+++ app/src/main/java/com/jay/exersiceinputdemo/MainActivity.java
@@ -1,35 +1,40 @@
 package com.jay.exersiceinputdemo;
 
+import android.graphics.PointF;
 import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.jay.exersiceinputdemo.widget.FingerSlideFrameView;
+
+/**
+ * desc:默认:题图根布局的背景
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    //activity_main的根布局:题图为背景
     private View container;
     private Rect rect;
+    private FingerSlideFrameView fingerSlideFrame;
+    private int offsetY, offsetX;
+    private OnSlideFrameListener mOnSlideFrameListener = new OnSlideFrameListener() {
+        @Override
+        public void onSlideFrameListener(PointF downPoint, PointF upPoint) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        container = findViewById(R.id.container);
-        container.post(new Runnable() {
-            @Override
-            public void run() {
-                rect = new Rect();
-                container.getLocalVisibleRect(rect);
-                Log.d(TAG, "container的宽高=" + rect.toString());
-            }
-        });
+        fingerSlideFrame = findViewById(R.id.finger_slide_frame);
+        fingerSlideFrame.setOnSlideFrameListener(mOnSlideFrameListener);
 
-
-
+        offsetX = fingerSlideFrame.getLeft();
+        offsetY = fingerSlideFrame.getTop();
     }
 }
--- app/src/main/java/com/jay/exersiceinputdemo/widget/FingerSlideFrameView.java
+++ app/src/main/java/com/jay/exersiceinputdemo/widget/FingerSlideFrameView.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.jay.exersiceinputdemo.OnSlideFrameListener;
+
 /**
  * Created by jj on 2019/3/23.
  * desc:可手指滑动画出一个矩形框,并记录该框在本view的位置及尺寸
@@ -21,10 +23,15 @@ public class FingerSlideFrameView extends View {
 
     private static final String TAG = FingerSlideFrameView.class.getSimpleName();
     private static final int DRAW_INTERVAL = 9;
+    private static final float CIRCLE_RADIO = 9;
     private PointF downPoint;
     private int moveCount;
     private Rect rect;
     private Paint paint = new Paint();
+    private boolean isInside4Circle, isInsideRect;
+    private int indexOfInsideCircle = -1;
+    private Rect mParentRect;
+    private OnSlideFrameListener mOnSlideFrameListener;
 
     public FingerSlideFrameView(Context context) {
         this(context, null);
@@ -40,10 +47,10 @@ public class FingerSlideFrameView extends View {
     }
 
     private void initPaint() {
-        Log.d(TAG, "initPaint");
         paint.setAntiAlias(true);
-        paint.setColor(Color.WHITE);
+        paint.setColor(Color.BLACK);
         paint.setStrokeWidth(3F);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
@@ -51,23 +58,30 @@ public class FingerSlideFrameView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                Log.d(TAG, "ACTION_DOWN");
                 downPoint = new PointF(event.getX(), event.getY());
+                if (rect != null) {
+                    isInside4Circle = isInside4Circle(downPoint);
+                    isInsideRect = isInsideRect(downPoint);
+                    Log.d(TAG, "是否在圆中?" + isInside4Circle + "//是否在矩形中?" + isInsideRect);
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "ACTION_MOVE");
                 moveCount++;
                 if (moveCount == DRAW_INTERVAL) {
                     moveCount = 0;
-                    drawRect(downPoint, new PointF(event.getX(), event.getY()));
+                    drawRect(new PointF(event.getX(), event.getY()));
                 }
 
                 break;
             case MotionEvent.ACTION_UP:
                 PointF upPoint = new PointF(event.getX(), event.getY());
-                drawRect(downPoint, upPoint);
-                Log.d(TAG, "框的位置:左=" + downPoint.x + "//上=" + downPoint.y + "//右=" + upPoint.x + "//下=" + upPoint.y);
+                drawRect(upPoint);
 
+                isInside4Circle = false;
+                isInsideRect = false;
+                indexOfInsideCircle = -1;
+
+                mOnSlideFrameListener.onSlideFrameListener(downPoint, upPoint);
                 break;
             default:
                 break;
@@ -75,17 +89,146 @@ public class FingerSlideFrameView extends View {
         return true;
     }
 
-    private void drawRect(PointF downPoint, PointF movePoint) {
-        rect = new Rect((int) downPoint.x, (int) downPoint.y, (int) movePoint.x, (int) movePoint.y);
+    private void drawRect(PointF upPoint) {
+        if (rect == null) {
+            generateRect(downPoint, upPoint);
+        } else if (isInside4Circle) {
+            if (indexOfInsideCircle < 0) {
+                Log.e(TAG, "ontouchEvent ACTION_DOWN逻辑异常");
+            } else {
+                switch (indexOfInsideCircle) {
+                    case 0:
+                        rect = new Rect(
+                                (int) Math.min(upPoint.x, rect.right),
+                                (int) Math.min(upPoint.y, rect.bottom),
+                                (int) Math.max(upPoint.x, rect.right),
+                                (int) Math.max(upPoint.y, rect.bottom));
+                        break;
+                    case 1:
+                        rect = new Rect(
+                                (int) Math.min(upPoint.x, rect.left),
+                                (int) Math.min(upPoint.y, rect.bottom),
+                                (int) Math.max(upPoint.x, rect.left),
+                                (int) Math.max(upPoint.y, rect.bottom));
+                        break;
+                    case 2:
+                        rect = new Rect(
+                                (int) Math.min(upPoint.x, rect.right),
+                                (int) Math.min(upPoint.y, rect.top),
+                                (int) Math.max(upPoint.x, rect.right),
+                                (int) Math.max(upPoint.y, rect.top));
+                        break;
+                    case 3:
+                        rect = new Rect(
+                                (int) Math.min(upPoint.x, rect.left),
+                                (int) Math.min(upPoint.y, rect.top),
+                                (int) Math.max(upPoint.x, rect.left),
+                                (int) Math.max(upPoint.y, rect.top));
+                        break;
+                }
+            }
+        } else if (isInsideRect) {
+            int width = getWidth();
+            int height = getHeight();
+            float offSetX = upPoint.x - downPoint.x;
+            float offSetY = upPoint.y - downPoint.y;
+
+            //左移出界
+            if (rect.left + offSetX < 0) {
+                upPoint.x = downPoint.x - rect.left;
+            }
+            //上移出界
+            else if (rect.top + offSetY < 0) {
+                upPoint.y = downPoint.y - rect.top;
+            }
+            //右移出界
+            else if (rect.right + offSetX > width) {
+                upPoint.x = width + downPoint.x - rect.right;
+            }
+            //下移出界
+            else if (rect.bottom + offSetY > height) {
+                upPoint.y = height + downPoint.y - rect.bottom;
+            }
+
+            //rect四个点添加offset
+            float realOffSetX = upPoint.x - downPoint.x;
+            float realOffSetY = upPoint.y - downPoint.y;
+            rect.left += realOffSetX;
+            rect.top += realOffSetY;
+            rect.right += realOffSetX;
+            rect.bottom += realOffSetY;
+        } else {
+            generateRect(downPoint, upPoint);
+        }
+
         invalidate();
+
+        Log.d(TAG, "框的位置:左=" + downPoint.x + "//上=" + downPoint.y + "//右=" + upPoint.x + "//下=" + upPoint.y);
+    }
+
+    /**
+     * action_down点不在既有的4个圆内,确定是否在矩形框内
+     */
+    private boolean isInsideRect(PointF downPoint) {
+        return downPoint.x > rect.left && downPoint.x < rect.right && downPoint.y > rect.top && downPoint.y < rect.bottom;
+    }
+
+    /**
+     * 确定action_down的点是否在既有的4个圆内,在的话确定在哪个圆内
+     */
+    private boolean isInside4Circle(PointF downPoint) {
+        float x = downPoint.x;
+        float y = downPoint.y;
+
+        //左上点
+        if (Math.sqrt((x - rect.left) * (x - rect.left) + (y - rect.top) * (y - rect.top)) < CIRCLE_RADIO) {
+            indexOfInsideCircle = 0;
+
+            //右上点
+        } else if (Math.sqrt((x - rect.right) * (x - rect.right) + (y - rect.top) * (y - rect.top)) < CIRCLE_RADIO) {
+            indexOfInsideCircle = 1;
+
+            //左下点
+        } else if (Math.sqrt((x - rect.left) * (x - rect.left) + (y - rect.bottom) * (y - rect.bottom)) < CIRCLE_RADIO) {
+            indexOfInsideCircle = 2;
+
+            //右下点
+        } else if (Math.sqrt((x - rect.right) * (x - rect.right) + (y - rect.bottom) * (y - rect.bottom)) < CIRCLE_RADIO) {
+
+            indexOfInsideCircle = 3;
+        }
+        Log.d(TAG, "在哪个圆圈?" + indexOfInsideCircle);
+        return indexOfInsideCircle >= 0;
+    }
+
+    /**
+     * (重)生成框
+     */
+    private void generateRect(PointF downPoint, PointF movePoint) {
+        int minX = (int) Math.min(downPoint.x, movePoint.x);
+        int maxX = (int) Math.max(downPoint.x, movePoint.x);
+        int minY = (int) Math.min(downPoint.y, movePoint.y);
+        int maxY = (int) Math.max(downPoint.y, movePoint.y);
+        if (Math.abs(minX - maxX) < CIRCLE_RADIO || Math.abs(minY - maxY) < CIRCLE_RADIO) {
+            rect = null;
+            return;
+        }
+        rect = new Rect(minX, minY, maxX, maxY);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.d(TAG, "onDraw");
         if (rect == null)
             return;
         canvas.drawRect(rect, paint);
+        canvas.drawCircle(rect.left, rect.top, CIRCLE_RADIO, paint);
+        canvas.drawCircle(rect.left, rect.bottom, CIRCLE_RADIO, paint);
+        canvas.drawCircle(rect.right, rect.top, CIRCLE_RADIO, paint);
+        canvas.drawCircle(rect.right, rect.bottom, CIRCLE_RADIO, paint);
+    }
+
+    public void setOnSlideFrameListener(OnSlideFrameListener onSlideFrameListener) {
+        mOnSlideFrameListener = onSlideFrameListener;
     }
 }
